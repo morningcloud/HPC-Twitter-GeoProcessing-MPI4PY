99abf0250897502d96ba371b714c0c977f594ad4
Ghawady Ehmaid <ghawadi@yahoo.com>
2019-04-09 19:57:51 +1000

    final run results

--- BatchJobs/MPIBatchRun1x1Physical.sh
+++ BatchJobs/MPIBatchRun1x1Physical.sh
@@ -4,5 +4,5 @@
 #SBATCH --partition=physical
 #SBATCH --ntasks-per-node=1
 module load Python/3.4.3-goolf-2015a
-mpiexec -np 1 python finalscript.py
+mpiexec python finalscript.py
 
--- BatchJobs/MPIBatchRun1x8Physical.sh
+++ BatchJobs/MPIBatchRun1x8Physical.sh
@@ -4,5 +4,5 @@
 #SBATCH --partition=physical
 #SBATCH --ntasks-per-node=8
 module load Python/3.4.3-goolf-2015a
-mpiexec -np 8 python finalscript.py
+mpiexec python finalscript.py
 
